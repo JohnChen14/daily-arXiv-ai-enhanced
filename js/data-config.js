30b4c51bcba16fff89426c6d396bbc951d015d88
John Chen <64292415+JohnChen14@users.noreply.github.com>
2026-02-15 14:08:11 -0500

    Update data-config.js
    change back

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'HaoChen14',
+    repoOwner: 'dw-dengwei',
 
     /**
      * GitHub repository name
